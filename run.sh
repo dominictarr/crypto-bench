7b39e7f17a6e32e1136f09b8e35b63c1da81881c
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-27 15:20:33 +0700

    use dat-table

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+PATH=node_modules/.bin:$PATH
+
 set -e
 
 bench () {
@@ -25,56 +27,55 @@ all () {
 
 graphs () {
   ls ./../dat-table/bin.js
-  table='../dat-table/bin.js'
 
 
   # sha hashing increasing file size.
 
-  $table output/hash/*-sha1.csv -c 1,3 \
+  dat-table output/hash/*-sha1.csv -c 1,3 \
   | line-graph --width 600 --height 400 --title 'time to SHA1 largish file' \
   > graphs/hash-sha1.png
 
-  $table output/hash/*-sha256.csv -c 1,3 \
+  dat-table output/hash/*-sha256.csv -c 1,3 \
   | line-graph --width 600 --height 400 --title 'time to SHA256 largish file' \
   > graphs/hash-sha256.png
 
   # sha hashing bytes/ms
 
-  $table output/hash/*-sha1.csv -c 1,2 \
+  dat-table output/hash/*-sha1.csv -c 1,2 \
   | line-graph --width 600 --height 400 --title 'bytes/ms SHA1 hashed' \
   > graphs/hash-ops-sha1.png
 
-  $table output/hash/*-sha256.csv -c 1,2 \
+  dat-table output/hash/*-sha256.csv -c 1,2 \
   | line-graph --width 600 --height 400 --title 'bytes/ms SHA256 hashed' \
   > graphs/hash-ops-sha256.png
 
   # key derivation
 
-  $table output/pbkdf2/*-sha1.csv -c 0,2 \
+  dat-table output/pbkdf2/*-sha1.csv -c 0,2 \
   | line-graph --width 600 --height 400 --title 'pbkdf2(sha1) iterations per millisecond' \
   > graphs/pbkdf2-ops-sha1.png
 
-  $table output/pbkdf2/*-sha256.csv -c 0,2 \
+  dat-table output/pbkdf2/*-sha256.csv -c 0,2 \
   | line-graph --width 600 --height 400 --title 'pbkdf2(sha256) iterations per millisecond' \
   > graphs/pbkdf2-ops-sha256.png
 
   # key derivation, iterations / ms
 
-  $table output/pbkdf2/*-sha1.csv -c 0,1 \
+  dat-table output/pbkdf2/*-sha1.csv -c 0,1 \
   | line-graph --width 600 --height 400 --title 'pbkdf2(sha1) increasing iterations' \
   > graphs/pbkdf2-sha1.png
 
-  $table output/pbkdf2/*-sha256.csv -c 0,1 \
+  dat-table output/pbkdf2/*-sha256.csv -c 0,1 \
   | line-graph --width 600 --height 400 --title 'pbkdf2(sha256) increasing iterations' \
   > graphs/pbkdf2-sha256.png
 
   # zoomed in on the very start of the file hash graph
 
-  $table output/hash/*-sha1.csv  -c 1,2 -s 0,30 \
+  dat-table output/hash/*-sha1.csv  -c 1,2 -s 0,30 \
   | line-graph --width 600 --height 400 --title 'sha1 hashing small values' \
   > graphs/small-hash-sha1.png
 
-  $table output/hash/*-sha256.csv  -c 1,2 -s 0,30 \
+  dat-table output/hash/*-sha256.csv  -c 1,2 -s 0,30 \
   | line-graph --width 600 --height 400 --title 'sha256 hashing small values' \
   > graphs/small-hash-sha256.png
 
