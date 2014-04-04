bd7b8d07e345b1ad5c77ac33a14c59655e6f3488
Dominic Tarr <dominic.tarr@gmail.com>
2014-04-04 22:28:24 +1300

    add more hash libraries

--- bench-hash.js
+++ bench-hash.js
@@ -40,7 +40,50 @@ var libs = {
     return function (data) {
       return crypto.createHash(alg).update(data).digest('hex')
     }
+  },
+  jshashes: function (alg) {
+    var jshashes = require('jshashes')
+    if (alg == 'ripemd160') alg = 'RMD160'
+      
+    var Hash = jshashes[alg.toUpperCase()]
+    var hash = new Hash()
+    return function (data) {
+      return hash.hex(data.toString('binary'))
+    }
+  },
+  cryptomx: function (alg) {
+    if(!/^(sha256|ripemd)$/.test(alg))  
+      throw new Error('algorithm '+alg+' is not implemented by cryptomx library')
+    return function (data) {      
+      return require('cryptomx')[alg](data.toString('binary'))
+    }
+  },
+  blake2s: function (alg) {
+    if(alg != 'blake2s')
+      throw new Error('algorithm '+alg+' is not implemented by blake2s library')
+    var Blake2s = require('blake2s')
+    return function (data) {
+      return new Blake2s().update(data).digest('hex')
+    }
+  },
+  'git-sha1': function (alg) {
+    if(alg != 'sha1') throw new Error('algorithm '+alg+'is not implemented by git-sha1 library')
+    var sha1 = require('git-sha1')
+    return function (data) {
+      return sha1(data)
+    }
+  },
+  'rusha': function (alg) {
+    if(alg != 'sha1') throw new Error('algorithm '+alg+'is not implemented by git-sha1 library')
+    var Rusha = require('rusha')
+    var sha1 = new Rusha()
+    return function (data) {
+      return sha1.digestFromBuffer(data)
+    }
+
+
   }
+
 }
 
 var prev = 0
@@ -49,6 +92,7 @@ var hash = libs[lib](alg)
 //for(var i = 79; i <= 80; i++) {
   console.log('run (N), input-size (bytes), ops (bytes/ms), time (ms)')
 var i = 1
+
 ;(function loop () {
   if(i > 80) return
   var n = Math.round(Math.pow(Math.pow(M, 1/80), i))
